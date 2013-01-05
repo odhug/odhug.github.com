f32f8620ae870410fc987ca2b09f1577f84ea63a
Roman Cheplyaka <roma@ro-che.info>
2013-01-06 01:25:41 +0200

    Add events.js

---
+++ events.js
@@ -0,0 +1,2 @@
+var eventList = [{"eventDescription":"<strong><a href=\"todo\">Октябрьская встреча</a></strong>\n","eventDate":"2012-10-04"},{"eventDescription":"<strong><a href=\"todo\">Ноябрьская встреча</a></strong>\n","eventDate":"2012-11-08"},{"eventDescription":"<strong><a href=\"todo\">Декабрьская встреча</a></strong>\n","eventDate":"2012-12-16"}]
+var noFutureEvents = "Нет предстоящих событий"
